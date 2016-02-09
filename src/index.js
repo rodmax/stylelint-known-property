4665f2d978beb046359c702adb5845b9498269c6
max.rodionov <max.rodionov@synesis.ru>
2016-02-09 18:45:05 +0300

    integrate with stylelint 4.x(incomplete)

--- src/index.js
+++ src/index.js
@@ -1,61 +1,31 @@
 var stylelint   = require('stylelint');
 var report      = stylelint.utils.report;
 
-var properties  = require('./../data');
-var messages    = require('./messages');
-
+var vendor = require("postcss").vendor;
+var stylelint   = require('stylelint');
 var ruleName    = 'known-property';
-var browserPrefixPattern = new RegExp('^-(webkit|moz|o|ms)-(.*)');
+var knownProperties  = require('./../data');
+var messages = stylelint.utils.ruleMessages(ruleName, {
+    rejected: 'Nesting exceeds maximum nesting depth',
+});
 
-function reject(result, node, type) {
-  report({
-    message: messages(type, node.prop),
-    node: node,
-    result: result,
-    ruleName: ruleName
-  });
+function knownPropertyPlugin(options) {
+    return function validate(root, result) {
+
+        root.walkDecls(function (decl) {
+            var prop = decl.prop;
+            if (knownProperties.indexOf(vendor.unprefixed(prop)) === -1) {
+                report({
+                    message: 'Unknown property: ' + prop,
+                    node: decl,
+                    result: result,
+                    ruleName: ruleName
+                });
+            }
+        });
+    };
 }
 
-function propertyExists (prop) {
-  return properties.indexOf(prop) > -1;
-}
-
-function hasBrowserPrefix (prop) {
-  return !!prop.match(browserPrefixPattern);
-}
-
-function removeBrowserPrefix (prop) {
-  return prop.replace(browserPrefixPattern, function (matches, prefix, propName) {
-    return propName;
-  });
-}
-
-function validate (result, whitelist, blacklist) {
-  return function (decl) {
-    var prop = decl.prop;
-
-    if (whitelist && whitelist.indexOf(prop) !== -1) {
-      return;
-    }
-
-    if (blacklist && blacklist.indexOf(prop) !== -1) {
-      return reject(result, decl, 'blacklisted');
-    }
-
-    if (propertyExists(prop)) {
-      return;
-    }
-
-    if (hasBrowserPrefix(prop) && propertyExists(removeBrowserPrefix(prop))) {
-      return;
-    }
-        
-    return reject(result, decl, 'unknown');
-  };
-}
-
-module.exports = function(whitelist, blacklist) {
-  return function(root, result) {
-    root.walkDecls(validate(result, whitelist, blacklist));
-  };
-};
+module.exports = stylelint.createPlugin(ruleName, knownPropertyPlugin);
+module.exports.ruleName = ruleName;
+module.exports.messages = messages;
